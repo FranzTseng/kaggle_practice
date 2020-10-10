444641cf281ff563c0ba34c58ad99dd502b8835b
FranzTseng <blume918@gmail.com>
2020-10-09 22:01:21 -0400

    add OOP_club

---
+++ python/OOP_club.py
@@ -0,0 +1,17 @@
+
+class Club:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def __getitem__(self, i):
+        return self.players[i]
+
+    def __repr__(self):
+        return f"Club {self.name}: {self.players}"
+
+    def __len__(self):
+        return len(self.players)
+
+    def __str__(self):
+        return f"Club {self.name} with {len(self)} players"
